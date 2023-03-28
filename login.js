5e918f92dd09e91684971ec853fbc5f4f599effb
Mikołaj Miszka <mikolaj150878@gmail.com>
2023-03-28 12:37:53 +0200

    First commit

---
+++ login.js
@@ -0,0 +1,25 @@
+function validateForm() {
+  const username = document.getElementById("username").value;
+  const password = document.getElementById("password").value;
+  let isValid = true;
+
+  const usernameError = document.getElementById("username-error");
+  const passwordError = document.getElementById("password-error");
+
+  usernameError.textContent = "";
+  passwordError.textContent = "";
+
+  if (username === "") {
+    usernameError.textContent = "Nazwa użytkownika nie może być pusta";
+    isValid = false;
+  }
+
+  if (password === "") {
+    passwordError.textContent = "Hasło nie może być puste";
+    isValid = false;
+  }
+
+  return isValid;
+}
+
+  
---
+++ login.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>Login Form</title>
+    <link rel="stylesheet" href="style.css">
+  </head>
+  <body>
+    <div class="form-container">
+      <h2>Zaloguj się</h2>
+      <form id="login-form" onsubmit="return validateForm()">
+        <div class="form-group">
+          <label for="username">Nazwa użytkownika:</label>
+          <input type="text" id="username" name="username">
+          <span id="username-error" class="error-message"></span>
+        </div>
+        <div class="form-group">
+          <label for="password">Hasło:</label>
+          <input type="password" id="password" name="password">
+          <span id="password-error" class="error-message"></span>
+        </div>
+        <input type="submit" value="zaloguj się">
+      </form>
+    </div>
+    <script src="login.js"></script>
+  </body>
+</html>
