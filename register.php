60435316490586f8e2712e70cb199d9b47d02ae8
mikolajroot <129083622+mikolajroot@users.noreply.github.com>
2023-04-05 15:19:33 +0200

    update

--- register.php
+++ register.php
@@ -1,37 +1,42 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login Form</title>
-    <link rel="stylesheet" href="login.css">
-  </head>
-  <body>
-    <div class="form-container">
-      <h2>Zarejestruj się</h2>
-      <form id="login-form" onsubmit="return validateForm()" action="register.php" method="get">
-        <div class="form-group">
-          <label for="username">Nazwa użytkownika:</label>
-          <input type="text" id="username" name="username-register">
-          <span id="username-error" class="error-message"></span>
-        </div>
-        <div class="form-group">
-          <label for="password">Hasło:</label>
-          <input type="password" id="password" name="password-register">
-          <span id="password-error" class="error-message"></span>
-        </div>
-        <div class="form-group">
-          <label for="password">Powtórz hasło:</label>
-          <input type="password" id="password-repeat" name="password-repeat">
-          <span id="password-repeat-error" class="error-message"></span>
-        </div>
-        <input type="submit" value="zajerestruj się">
-        <input type="reset" value="Reset" id="reset">
-      </form>
-    </div>
-    <?php
-    //TODO: skrypt do rejestracji
-    ?>
-    <script src="register.js"></script>
-  </body>
-</html>
+<?php
+// Connect to the database
+$host = "localhost"; // replace with your host name
+$user = "root"; // replace with your database username
+$password = ""; // replace with your database password
+$database = "inf"; // replace with your database name
+
+$con = mysqli_connect($host, $user, $password, $database);
+
+// Check connection
+if (mysqli_connect_errno()) {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  exit();
+}
+
+// Retrieve form data
+$username = $_POST['username-register'];
+$password = $_POST['password-register'];
+$password_repeat = $_POST['password-repeat'];
+
+// Check if password and password repeat match
+if ($password != $password_repeat) {
+  echo "Hasła nie pasują.";
+  exit();
+}
+
+// Hash the password for security
+// $password_hashed = password_hash($password, PASSWORD_DEFAULT);
+
+// Insert the user into the database
+$sql = "INSERT INTO users (username, password) VALUES ('$username', '$password')";
+
+if (mysqli_query($con, $sql)) {
+  echo "User registered successfully.";
+  header("Location: login.html");
+} else {
+  echo "Error: " . $sql . "<br>" . mysqli_error($con);
+}
+
+// Close the database connection
+mysqli_close($con);
+?>
